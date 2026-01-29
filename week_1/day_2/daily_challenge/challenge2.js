309387f59584a18bf702548029bedcdaf6855304
zel-hach <zineb.ell.zee@gmail.com>
2026-01-29 12:06:50 +0100

    add challenge2 day2

---
+++ week_1/day_2/daily_challenge/challenge2.js
@@ -0,0 +1,15 @@
+const readline = require('readline-sync');
+const text = readline.question("Please enter a sentence: ");
+if(!text){
+    console.log("You did not enter a sentence!");
+    process.exit(1);
+}
+const TextTable = text.split(' ');
+const longestElement = TextTable.reduce((longest, current) => longest.length >= current.length ? longest : current, "");
+console.log("Reversed sentence: ",longestElement.length);
+
+displayseparatedWords(longestElement.length + 4);
+for(let i=0 ; i<TextTable.length; i++){
+    console.log('* ' + TextTable[i]  + ' '.repeat(longestElement.length - TextTable[i].length) + ' *');
+}
+displayseparatedWords(longestElement.length + 4);
--- week_1/day_2/daily_challenge/challenges.js
+++ week_1/day_2/daily_challenge/challenges.js
@@ -1,37 +1,46 @@
 // ====== Daily Challenge 1
 
-const solarSystem = [
-  { planet: "Mercury", moons: [] },
-  { planet: "Venus", moons: [] },
-  { planet: "Earth", moons: ["Moon"] },
-  { planet: "Mars", moons: ["Phobos", "Deimos"] },
-  { planet: "Jupiter", moons: ["Io", "Europa", "Ganymede", "Callisto"] },
-  { planet: "Saturn", moons: ["Titan", "Enceladus"] },
-  { planet: "Uranus", moons: ["Titania", "Oberon"] },
-  { planet: "Neptune", moons: ["Triton"] }
-];
+// const solarSystem = [
+//   { planet: "Mercury", moons: [] },
+//   { planet: "Venus", moons: [] },
+//   { planet: "Earth", moons: ["Moon"] },
+//   { planet: "Mars", moons: ["Phobos", "Deimos"] },
+//   { planet: "Jupiter", moons: ["Io", "Europa", "Ganymede", "Callisto"] },
+//   { planet: "Saturn", moons: ["Titan", "Enceladus"] },
+//   { planet: "Uranus", moons: ["Titania", "Oberon"] },
+//   { planet: "Neptune", moons: ["Triton"] }
+// ];
 
-const section = document.getElementsByClassName("listPlanets");
-solarSystem.map((item) => 
-    {
-        const div = document.createElement("div");
-        const p = document.createElement("p");
-        const dividend = document.createElement("div");
-        dividend.className = "dividend";
-        p.innerText = item.planet;
-        div.appendChild(p);
-        for(let moon of item.moons){
-            const moonDiv =  document.createElement("div");
-            moonDiv.className = "moon";
-            moonDiv.innerText = moon;
-            moonDiv.style.color = "brown";
-            dividend.appendChild(moonDiv);
-        }
-        div.className = "planet";
-        div.classList.add(item.planet.toLowerCase());
-        div.appendChild(dividend);
-        section[0].appendChild(div);
-    });
+// const section = document.getElementsByClassName("listPlanets");
+// solarSystem.map((item) => 
+//     {
+//         const div = document.createElement("div");
+//         const p = document.createElement("p");
+//         const dividend = document.createElement("div");
+//         dividend.className = "dividend";
+//         p.innerText = item.planet;
+//         div.appendChild(p);
+//         for(let moon of item.moons){
+//             const moonDiv =  document.createElement("div");
+//             moonDiv.className = "moon";
+//             moonDiv.innerText = moon;
+//             moonDiv.style.color = "brown";
+//             dividend.appendChild(moonDiv);
+//         }
+//         div.className = "planet";
+//         div.classList.add(item.planet.toLowerCase());
+//         div.appendChild(dividend);
+//         section[0].appendChild(div);
+//     });
 // ====== Daily Challenge 2
+
+function displayseparatedWords(length){
+    let line = '';
+    for(let i=0 ; i<length; i++){
+        line += '*';
+    }
+    console.log(line);
+}
+
 
 // ====== Daily Challenge 3

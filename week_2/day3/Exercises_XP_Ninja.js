bbba401c621bc4bc528d9d5b80f580a2a05de3b0
zel-hach <zineb.ell.zee@gmail.com>
2026-02-17 14:25:32 +0000

    add Exercices_xp_Ninja.js

---
+++ week_2/day3/Exercises_XP_Ninja.js
@@ -0,0 +1,15 @@
+class Bird {
+  constructor() {
+    console.log("I'm a bird. 🦢");
+  }
+}
+
+class Flamingo extends Bird {
+  constructor() {
+    console.log("I'm pink. 🌸");
+    super();
+  }
+}
+
+const pet = new Flamingo();// I'm pink. 🌸
+                        // I'm a bird. 🦢

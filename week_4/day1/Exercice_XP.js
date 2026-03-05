6c2b04d21adfbbd7268bf63a63acc426546da686
zel-hach <zineb.ell.zee@gmail.com>
2026-03-05 10:22:40 +0000

    add Exercice_XP day1 week4

---
+++ week_4/day1/Exercice_XP.js
@@ -0,0 +1,30 @@
+db.students.find().pretty()
+////=>result
+{
+  _id: ObjectId('69a843a7e52b2865b82305a3'),
+  name: 'Yassine',
+  age: 19,
+  city: 'Casablanca',
+  major: 'Generative AI'
+}
+{
+  _id: ObjectId('69a843a7e52b2865b82305a5'),
+  name: 'Kaito',
+  age: 22,
+  city: 'Tokyo',
+  major: 'Web Dev'
+}
+{
+  _id: ObjectId('69a843a7e52b2865b82305a6'),
+  name: 'Elena',
+  age: 31,
+  city: 'Madrid',
+  major: 'Data Science'
+}
+{
+  _id: ObjectId('69a843a7e52b2865b82305a7'),
+  name: 'Marcus',
+  age: 24,
+  city: 'New York',
+  major: 'Generative AI'
+}
